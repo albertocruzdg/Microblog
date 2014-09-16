cbce89806e595cce4a6d89d68e427169605cfbdf
Alberto <cruz.alberto92@gmail.com>
2014-09-16 15:45:42 -0300

    Se agrego la posibilidad de agregar nuevos posts

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from posts import views
 
 urlpatterns=patterns('',
 	url(r'^$',views.index, name='index'),
-	# url(r'^/post/$',views.post, name='post'),
+	url(r'^post/$',views.post, name='post'),
 	url(r'^(?P<post_id>\d+)/$',views.detail, name='detail'),
 	url(r'^(?P<post_id>\d+)/comment/$',views.comment, name='comment'),
 )
--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 from .models import Post
 # Create your views here.
 
 def index(request):
-	lastest_post_list=Post.objects.order_by('-date')[:5]
+	lastest_post_list=Post.objects.order_by('-date')[:5]	
 	context={'lastest_post_list' : lastest_post_list}
 	return render(request,'posts/index.html',context)
+
+def post(request):
+	p=Post(
+		user=request.user,
+		text=request.POST['text'],
+		date=timezone.now(),
+		)
+	p.save()
+	return HttpResponseRedirect(reverse('posts:index'))
 
 def detail(request, post_id):
 	post=get_object_or_404(Post, pk=post_id)
