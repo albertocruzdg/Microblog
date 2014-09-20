bf6bbe306e396315d51308033c1259f7fd0c5eb2
Alberto <cruz.alberto92@gmail.com>
2014-09-19 21:58:34 -0300

    Backend de DB

--- microblog/settings.py
+++ microblog/settings.py
@@ -58,9 +58,18 @@ WSGI_APPLICATION = 'microblog.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ec2-54-197-241-67.compute-1.amazonaws.com',
+        'USER': 'ec2-54-197-241-67.compute-1.amazonaws.com',
+        'PASSWORD': 'ec2-54-197-241-67.compute-1.amazonaws.com',
+        'HOST': 'ec2-54-197-241-67.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
